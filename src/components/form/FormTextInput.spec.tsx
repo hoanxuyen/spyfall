b939a8ecc4e36ea0113ab7d40fd92b2c95ddb751
hoanxuyen <hoanxuyen98@gmail.com>
2024-06-21 16:59:26 +0700

    feat:TextInput specs updated with beforeEach()

--- src/components/form/FormTextInput.spec.tsx
+++ src/components/form/FormTextInput.spec.tsx
@@ -4,66 +4,51 @@ import userEvent from "@testing-library/user-event";
 import FormTextInput from "./FormTextInput";
 
 describe("FormNumberInput Component", () => {
-  it("should render the input and label in the form", () => {
+  const onSubmit = vi.fn();
+  const user = userEvent.setup();
+  const ElementTestIds = {
+    input: "formInputElement",
+    label: "formInputLabel",
+    error: "formInputError",
+  };
+  beforeEach(() => {
     render(
-      <FormContainer onSubmit={vi.fn()}>
+      <FormContainer onSubmit={onSubmit}>
         <FormTextInput
           label="Label"
           name="test"
           type="number"
           rules={{ required: "Error" }}
         />
+        <button type="submit">Submit</button>
       </FormContainer>
     );
+  });
 
-    const inputElement = screen.getByTestId("formInputElement");
-    const labelElement = screen.getByTestId("formInputLabel");
+  it("should render the input and label in the form", () => {
+    const inputElement = screen.getByTestId(ElementTestIds.input);
+    const labelElement = screen.getByTestId(ElementTestIds.label);
 
     expect(labelElement).toHaveTextContent("Label");
     expect(inputElement).toBeInTheDocument();
   });
 
   it("should show error message on submit when input is empty", async () => {
-    const onSubmit = vi.fn();
-    render(
-      <FormContainer onSubmit={onSubmit}>
-        <FormTextInput
-          label="Label"
-          name="test"
-          type="number"
-          rules={{ required: "Error" }}
-        />
-        <button type="submit">Submit</button>
-      </FormContainer>
-    );
-
     const buttonElement = screen.getByRole("button");
-    const user = userEvent.setup();
     await user.click(buttonElement);
 
-    const errorElement = screen.getByTestId("formInputError");
+    const errorElement = screen.getByTestId(ElementTestIds.error);
     expect(errorElement).toBeInTheDocument();
     expect(errorElement).toHaveTextContent("Error");
   });
 
   it("should call onSubmit function when the form is submitted", async () => {
-    const onSubmit = vi.fn();
-    render(
-      <FormContainer onSubmit={onSubmit}>
-        <FormTextInput
-          label="Label"
-          name="test"
-          type="number"
-          rules={{ required: "Error" }}
-        />
-        <button type="submit">Submit</button>
-      </FormContainer>
-    );
-    const inputElement = screen.getByTestId("formInputElement");
+    const inputElement = screen.getByTestId(ElementTestIds.input);
     const buttonElement = screen.getByRole("button");
-    const user = userEvent.setup();
+
     await user.type(inputElement, "123");
     await user.click(buttonElement);
+
     expect(onSubmit).toHaveBeenCalled();
   });
 });
