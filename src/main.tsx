9b3ff682a873bb4006341a73cc89ca2a5116ea37
hoanxuyen <hoanxuyen98@gmail.com>
2024-07-03 22:05:02 +0700

    core: Added basename for react-router

--- src/main.tsx
+++ src/main.tsx
@@ -11,60 +11,62 @@ import SpyLobby from "./components/SpyLobby.tsx";
 import SpyDiscuss from "./components/SpyDiscuss.tsx";
 import SpyReveal from "./components/SpyReveal.tsx";
 import PageFadeIn from "./components/PageFadeIn.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SpyGlobalLayout />,
-    children: [
-      {
-        path: "/",
-        element: (
-          <PageFadeIn>
-            <SpyMainMenu />
-          </PageFadeIn>
-        ),
-      },
-      {
-        path: "/lobby",
-        element: (
-          <PageFadeIn>
-            <SpyLobby />
-          </PageFadeIn>
-        ),
-      },
-      {
-        path: "/roles",
-        element: (
-          <PageFadeIn>
-            <SpyAnnounceRole />
-          </PageFadeIn>
-        ),
-      },
-      {
-        path: "/discuss",
-        element: (
-          <PageFadeIn>
-            <SpyDiscuss />
-          </PageFadeIn>
-        ),
-      },
-      {
-        path: "/reveal",
-        element: (
-          <PageFadeIn>
-            <SpyReveal />
-          </PageFadeIn>
-        ),
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <SpyGlobalLayout />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <PageFadeIn>
+              <SpyMainMenu />
+            </PageFadeIn>
+          ),
+        },
+        {
+          path: "/lobby",
+          element: (
+            <PageFadeIn>
+              <SpyLobby />
+            </PageFadeIn>
+          ),
+        },
+        {
+          path: "/roles",
+          element: (
+            <PageFadeIn>
+              <SpyAnnounceRole />
+            </PageFadeIn>
+          ),
+        },
+        {
+          path: "/discuss",
+          element: (
+            <PageFadeIn>
+              <SpyDiscuss />
+            </PageFadeIn>
+          ),
+        },
+        {
+          path: "/reveal",
+          element: (
+            <PageFadeIn>
+              <SpyReveal />
+            </PageFadeIn>
+          ),
+        },
+      ],
+    },
+  ],
+  { basename: "/spyfall" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
